e99b37bb669f25e49e06267119a9d1f5bb757af0
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2026-02-07 19:54:37 +0900

    feat: mini-nvim setting

---
+++ mini-nvim/init.lua
@@ -0,0 +1,134 @@
+local map = vim.api.nvim_set_keymap
+vim.g.mapleader = ' '
+map('i', 'jk', '<Esc>', { noremap = true })
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.clipboard:append({"unnamedplus"})
+vim.opt.clipboard = "unnamedplus"
+
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
+
+-- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
+local path_package = vim.fn.stdpath('data') .. '/site/'
+local mini_path = path_package .. 'pack/deps/start/mini.nvim'
+if not vim.uv.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = {
+    'git', 'clone', '--filter=blob:none',
+    'https://github.com/echasnovski/mini.nvim', mini_path
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+end
+
+-- Set up 'mini.deps' (customize to your liking)
+require('mini.deps').setup({ path = { package = path_package } })
+
+local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
+
+now(function()
+  require('mini.icons').setup()
+end)
+
+now(function()
+  require('mini.basics').setup()
+end)
+
+now(function()
+  require('mini.files').setup({
+      options = {
+          use_as_default_explorer = true,
+      }
+  })
+
+  local minifiles_toggle = function(...)
+    if not MiniFiles.close() then MiniFiles.open(...) end
+  end
+
+  local map_split = function(buf_id, lhs, direction)
+  local rhs = function()
+    -- Make new window and set it as target
+    local cur_target = MiniFiles.get_explorer_state().target_window
+    local new_target = vim.api.nvim_win_call(cur_target, function()
+      vim.cmd(direction .. ' split')
+      return vim.api.nvim_get_current_win()
+    end)
+
+    MiniFiles.set_target_window(new_target)
+
+    -- This intentionally doesn't act on file under cursor in favor of
+    -- explicit "go in" action (`l` / `L`). To immediately open file,
+    -- add appropriate `MiniFiles.go_in()` call instead of this comment.
+  end
+
+  -- Adding `desc` will result into `show_help` entries
+  local desc = 'Split ' .. direction
+  vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
+  end
+  
+  vim.api.nvim_create_user_command(
+    'Ex',
+    function()
+      MiniFiles.open()
+    end,
+    { desc = 'Open file exproler' }
+  )
+  vim.api.nvim_create_user_command(
+    'Ve',
+    function()
+      vim.cmd('vertical leftabove new')
+      MiniFiles.open()
+    end,
+    { desc = 'Vertical split and Open file exproler' }
+  )
+  vim.api.nvim_create_user_command(
+    'Se',
+    function()
+      vim.cmd('leftabove new')
+      MiniFiles.open()
+    end,
+    { desc = 'horizontal split and Open file exproler' }
+  )
+
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'MiniFilesBufferCreate',
+    callback = function(args)
+      local buf_id = args.data.buf_id
+      -- Tweak keys to your liking
+      map_split(buf_id, '<leader>s', 'belowright horizontal')
+      map_split(buf_id, '<leader>v', 'belowright vertical')
+      map_split(buf_id, '<leader>t', 'tab')
+    end,
+  })
+end)
+
+later(function()
+  add('https://github.com/vim-jp/vimdoc-ja')
+  -- Prefer Japanese as the help language
+  vim.opt.helplang:prepend('ja')
+end)
+
+later(function()
+  add('https://github.com/zbirenbaum/copilot.lua')
+  require('copilot').setup()
+end)
