018f25882309b57ff581ec70f49b195242a14662
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2026-02-09 14:49:44 +0900

    feat: mini.hipatterns & mini.extra colorize keywords and color code

--- mini-nvim/init.lua
+++ mini-nvim/init.lua
@@ -197,3 +197,20 @@ later(function()
   add('https://github.com/zbirenbaum/copilot.lua')
   require('copilot').setup()
 end)
+
+-- colorize specific word and color code
+later(function()
+  local hipatterns = require('mini.hipatterns')
+  local hi_words = require('mini.extra').gen_highlighter.words
+  hipatterns.setup({
+    highlighters = {
+      -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+      fixme = hi_words({ 'FIXME', 'Fixme', 'fixme' }, 'MiniHipatternsFixme'),
+      hack = hi_words({ 'HACK', 'Hack', 'hack' }, 'MiniHipatternsHack'),
+      todo = hi_words({ 'TODO', 'Todo', 'todo' }, 'MiniHipatternsTodo'),
+      note = hi_words({ 'NOTE', 'Note', 'note' }, 'MiniHipatternsNote'),
+      -- Highlight hex color strings (`#rrggbb`) using that color
+      hex_color = hipatterns.gen_highlighter.hex_color(),
+    },
+  })
+end)
