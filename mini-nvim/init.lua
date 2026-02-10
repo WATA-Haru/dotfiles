f12bf98b30b1bf88b40702a6486033fd03be3e43
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2026-02-10 23:38:32 +0900

    feat: add mini.bufremove command

--- mini-nvim/init.lua
+++ mini-nvim/init.lua
@@ -336,3 +336,15 @@ end)
 later(function()
   require('mini.tabline').setup()
 end)
+
+later(function()
+  require('mini.bufremove').setup()
+
+  vim.api.nvim_create_user_command(
+    'Bufdelete',
+    function()
+      MiniBufremove.delete()
+    end,
+    { desc = 'Remove buffer' }
+  )
+end)
