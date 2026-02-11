9baf79ebb4f26d14c004ead79263f927ca7ed68d
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2026-02-11 20:41:14 +0900

    feat: add BufWrite auto create directory autocmd

--- mini-nvim/init.lua
+++ mini-nvim/init.lua
@@ -22,6 +22,20 @@ vim.g.mapleader = ' '
 map('i', 'jk', '<Esc>', { noremap = true })
 map('n', 'gb', '<C-t>', { desc = 'go back' })
 
+-- https://vim-jp.org/vim-users-jp/2011/02/20/Hack-202.html
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*',
+  callback = function(event)
+    local dir = vim.fs.dirname(event.file)
+    local force = vim.v.cmdbang == 1
+    if vim.fn.isdirectory(dir) == 0
+        and (force or vim.fn.confirm('"' .. dir .. '" does not exist. Create?', "&Yes\n&No") == 1) then
+      vim.fn.mkdir(vim.fn.iconv(dir, vim.opt.encoding:get(), vim.opt.termencoding:get()), 'p')
+    end
+  end,
+  desc = 'Auto mkdir to save file'
+})
+
 vim.g.clipboard = {
   name = 'OSC 52',
   copy = {
