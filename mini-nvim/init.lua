f06ce1307e3cb9b0d93fb93486e50463db5c04d1
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2026-02-11 20:17:55 +0900

    feat: mini.map

--- mini-nvim/init.lua
+++ mini-nvim/init.lua
@@ -389,6 +389,8 @@ later(function()
 
       -- option toggle (mini.basics)
       { mode = 'n', keys = 'm' },
+      -- mini.map
+      { mode = 'n', keys = 'mm', desc = '+mini.map' },
     },
 
     clues = {
@@ -592,7 +594,7 @@ later(function()
   })
 end)
 
-later(function ()
+later(function()
   require('mini.jump2d').setup()
 end)
 
@@ -626,4 +628,21 @@ end)
 
 later(function()
   require('mini.move').setup()
+end)
+
+later(function()
+  local mini_map = require('mini.map')
+  mini_map.setup({
+    integrations = {
+      mini_map.gen_integration.builtin_search(),
+      mini_map.gen_integration.diff(),
+      mini_map.gen_integration.diagnostic(),
+    },
+    symbols = {
+      scroll_line = '▶',
+    }
+  })
+  vim.keymap.set('n', 'mmf', MiniMap.toggle_focus, { desc = 'MiniMap.toggle_focus' })
+  vim.keymap.set('n', 'mms', MiniMap.toggle_side, { desc = 'MiniMap.toggle_side' })
+  vim.keymap.set('n', 'mmt', MiniMap.toggle, { desc = 'MiniMap.toggle' })
 end)
