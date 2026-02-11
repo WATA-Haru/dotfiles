f658412c9f6e4dfcfff409be1ffd65bc69090a36
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2026-02-11 20:54:31 +0900

    feat: command mode <c-n> <c-p> mapping
    https://zenn.dev/kawarimidoll/books/6064bf6f193b51/viewer/6c77c3#lua%E9%96%A2%E6%95%B0%E3%82%92%E5%AE%9F%E8%A1%8C%E3%81%99%E3%82%8B%E4%BE%8B

--- mini-nvim/init.lua
+++ mini-nvim/init.lua
@@ -27,6 +27,14 @@ map('n', 'gb', '<C-t>', { desc = 'go back' })
 map('n', '-', '"_', { desc = 'black hole register' })
 map('x', '-', '"_', { desc = 'black hole register' })
 
+-- https://zenn.dev/kawarimidoll/books/6064bf6f193b51/viewer/6c77c3#lua%E9%96%A2%E6%95%B0%E3%82%92%E5%AE%9F%E8%A1%8C%E3%81%99%E3%82%8B%E4%BE%8B
+vim.keymap.set('c', '<c-n>', function()
+  return vim.fn.wildmenumode() == 1 and '<c-n>' or '<down>'
+end, { expr = true, desc = 'Select next' })
+vim.keymap.set('c', '<c-p>', function()
+  return vim.fn.wildmenumode() == 1 and '<c-p>' or '<up>'
+end, { expr = true, desc = 'Select previous' })
+
 -- https://vim-jp.org/vim-users-jp/2011/02/20/Hack-202.html
 vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*',
