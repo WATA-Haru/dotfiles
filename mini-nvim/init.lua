ad2d1515671baa00575781d74bc158bf4675e6c3
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2026-02-12 15:04:51 +0900

    fix: linux disable clipboard while pasting from os

--- mini-nvim/init.lua
+++ mini-nvim/init.lua
@@ -79,15 +79,15 @@ if vim.bool_fn.has('linux') then
       ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
     },
     paste = {
-      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+      ['+'] = none,
+      ['*'] = none,
     },
   }
 
   -- https://github.com/neovim/neovim/discussions/29350#discussioncomment-10299517
   if vim.env.TMUX ~= nil then
     local copy = { 'tmux', 'load-buffer', '-w', '-' }
-    local paste = { 'bash', '-c', 'tmux refresh-client -l && sleep 0.05 && tmux save-buffer -' }
+    -- local paste = { 'bash', '-c', 'tmux refresh-client -l && sleep 0.05 && tmux save-buffer -' }
     vim.g.clipboard = {
       name = 'tmux',
       copy = {
@@ -95,8 +95,8 @@ if vim.bool_fn.has('linux') then
         ['*'] = copy,
       },
       paste = {
-        ['+'] = paste,
-        ['*'] = paste,
+        ['+'] = none,
+        ['*'] = none,
       },
       cache_enabled = 0,
     }
