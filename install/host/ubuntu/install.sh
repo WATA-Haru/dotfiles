d98835d5bf644e441420c683c91383e6577a3ac1
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2026-02-23 23:21:12 +0900

    fix: add TODO

--- install/host/ubuntu/install.sh
+++ install/host/ubuntu/install.sh
@@ -96,44 +96,36 @@ prompt_chezmoi() {
 maybe_install "package-manager" "$CHECK_DIR/package-manager.sh" "$TOOLS_DIR/package-manager.sh"
 
 # check/base.shで存在チェック
-# if 存在する -> skip
-# if 存在しない -> 実行する? y/n
 maybe_install "base" "$CHECK_DIR/base.sh" "$TOOLS_DIR/base.sh"
 
 # check/gnome.shで存在チェック
-# if 存在する -> skip
-# if 存在しない -> 実行する? y/n
 maybe_install "gnome" "$CHECK_DIR/gnome.sh" "$TOOLS_DIR/gnome.sh"
+
+# TODO: gnomeでshutdownする場合は<Ctrl+C>でプロセスを終わらせてくださいのスクリプトを入れたい
 echo "shutdown required"
 
 # check/docker.shで存在チェック
-# if 存在する -> skip
-# if 存在しない -> 実行する? y/n
 maybe_install "docker" "$CHECK_DIR/docker.sh" "$TOOLS_DIR/docker.sh"
+
+# TODO: gnomeでshutdownする場合は<Ctrl+C>でプロセスを終わらせてくださいのスクリプトを入れたい
 echo "setup required"
 echo "shutdown required"
 
 ## check/lc.shで存在チェック
-# if 存在する -> skip
-# if 存在しない -> 実行する? y/n
 maybe_install "lc" "$CHECK_DIR/lc.sh" "$TOOLS_DIR/lc.sh"
+# TODO: gnomeでshutdownする場合は<Ctrl+C>でプロセスを終わらせてくださいのスクリプトを入れたい
 echo "shutdown required"
 
 ### check/fcitx5.shで存在チェック
-# if 存在する -> skip
-# if 存在しない -> 実行する? y/n
 maybe_install "fcitx5" "$CHECK_DIR/fcitx5.sh" "$TOOLS_DIR/fcitx5.sh"
-# echo setup required
-# echo shutdown required
+# TODO: gnomeでshutdownする場合は<Ctrl+C>でプロセスを終わらせてくださいのスクリプトを入れたい
+echo "setup required"
+echo "shutdown required"
 
 ### check/tools.shで存在チェック
-# if 存在する -> skip
-# if 存在しない -> 実行する? y/n
 maybe_install "dev-tools" "$CHECK_DIR/tools.sh" "$TOOLS_DIR/dev-tools.sh"
 
 ### check/daily-use.shで存在チェック
-# if 存在する -> skip
-# if 存在しない -> 実行する? y/n
 maybe_install "daily-use" "$CHECK_DIR/daily-use.sh" "$TOOLS_DIR/daily-use-packages.sh"
 
 # run
