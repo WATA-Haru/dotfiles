1ba2dca952fcfb7664b9cdcf2ac651977d0e10fe
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2026-02-28 18:30:28 +0900

    fix: tmux clipboard condition mistake

--- home/dot_config/nvim/lua/clipboard.lua
+++ home/dot_config/nvim/lua/clipboard.lua
@@ -1,4 +1,8 @@
-if vim.bool_fn.has('mac') then
+local isMac = vim.bool_fn.has('mac')
+local isLinux = vim.bool_fn.has('linux')
+local isTmux = vim.env.TMUX ~= nil
+
+if isMac then
   vim.g.clipboard = {
     name = 'mac',
     copy = {
@@ -12,21 +16,9 @@ if vim.bool_fn.has('mac') then
   }
 end
 
-if vim.bool_fn.has('linux') then
-  vim.g.clipboard = {
-    name = 'OSC 52',
-    copy = {
-      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-    },
-    paste = {
-      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-    },
-  }
-
+if isLinux then
+  if isTmux then
   -- https://github.com/neovim/neovim/discussions/29350#discussioncomment-10299517
-  if vim.env.TMUX ~= nil then
     local copy = { 'tmux', 'load-buffer', '-w', '-' }
     local paste = { 'bash', '-c', 'tmux refresh-client -l && sleep 0.05 && tmux save-buffer -' }
     vim.g.clipboard = {
@@ -41,5 +33,18 @@ if vim.bool_fn.has('linux') then
       },
       cache_enabled = 0,
     }
+  else
+    vim.g.clipboard = {
+      name = 'OSC 52',
+      copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+      },
+      paste = {
+        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+      },
+    }
   end
 end
+
